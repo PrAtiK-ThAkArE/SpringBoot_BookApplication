b252b5a2397fbb2b1e8023981b564080ecb99d4a
Prateek <Prateek@PRATIKTHAKARE>
2023-04-10 19:22:59 +0530

    Commit

---
+++ SpringBoot_BookApplication-1/src/main/java/com/book/SpringBootBookApplication1Application.java
@@ -0,0 +1,13 @@
+package com.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookApplication1Application.class, args);
+	}
+
+}
